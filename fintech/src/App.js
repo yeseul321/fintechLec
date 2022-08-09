844663ada89972ecc400016017db1defb4daf7be
yeseul321 <amy7806@gmail.com>
2022-08-09 16:10:04 +0900

    state 관련 코드 넣기 전

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Welcome from "./components/Welcome";
+import InputExample from "./components/InputExample";
 
 
 
@@ -8,10 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>안녕하세요</h1>
-      <Welcome username={"정예슬"} age={33}></Welcome>
-      <Welcome username={"홍길동"} age={33}></Welcome>
-      <Welcome username={"둘리"} age={33}></Welcome>
-      <Welcome username={"도우너"} age={33}></Welcome>
+      <InputExample></InputExample>
     </div>
   );
 }
---
+++ fintech/src/components/InputExample.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const InputExample = () => {
+  let userInput = "입력 받기전";
+  const [userInputWithState, setUserInputWithState] = useState("입력 받기전");
+
+  const handleChangeInput = (event) => {
+    const { value } = event.target;
+    console.log(value);
+    setUserInputWithState(value);
+  };
+
+  const handleClickButton = () => {
+    console.log("button click");
+  };
+
+  return (
+    <div>
+      <p>{userInputWithState}</p>
+      <input onChange={handleChangeInput}></input>
+      <button onClick={handleClickButton}>입력</button>
+    </div>
+  );
+};
+
+export default InputExample;
